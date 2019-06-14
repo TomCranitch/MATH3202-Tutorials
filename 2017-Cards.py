6f17c84be99fc5eb8475803d92fbd47950c04842
Tom Cranitch <tom@cranitch.com.au>
2019-06-14 11:14:32 +1000

    Start of prac exam

--- 2017-Cards.py
+++ 2017-Cards.py
@@ -1,10 +1,32 @@
-def V(c1, c2, c3, c4, drawn, rem):
 
-    unfilled = sum(c for c in [c1, c2, c3, c4] if c == -1)
-    if unfilled == 1:
-        return (10*c1 + c2) * (10*c3 + c4)
+def V(C, rem):
 
-    return max(
-        (1/len(rem)) * V(c1, c2, c3, c4, rem.remove(c))
-        for c in rem
-    )
+    unfilled = min(C)
+    if unfilled != -1:
+        return (10 *C[0] + C[1]) * (10 *C[2] + C[3])
+
+    expected = 0
+
+    for r in rem:
+        best = 10000
+        move = -1
+
+        for c in C:
+            if C[c] == -1:
+
+                a = C
+                a.insert(c+1, r)
+                a.pop(c)
+
+                result = V(a, [i for i in rem if i != r])
+
+                if result < best:
+                    best = result
+                    move = c
+
+        expected += (1/len(rem)) * best
+
+    return expected
+
+
+print(V([-1, -1, -1, -1], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
